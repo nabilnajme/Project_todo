2bdc68faa1b6d3b906bddb360a15211ab47782d6
Nabil <maizounabil@gmail.com>
2026-01-20 01:51:37 +0100

    just chilling

--- todo-app/src/TodoList.jsx
+++ todo-app/src/TodoList.jsx
@@ -1,13 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useState , useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-
 export default function TodoHome() {
   const location = useLocation();
-  const userName = location.state?.userName || "Guest";
+  const userName = location.state.userName;
 
   const todos = useSelector((state) => state);
   const navigate = useNavigate();
@@ -15,10 +14,11 @@ export default function TodoHome() {
   const inputRef = useRef(null);
   const [text, setText] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
- useEffect(() => {
+  useEffect(() => {
     inputRef.current.focus();
   }, []);
-  function addTodo() {
+  const addTodo = (e) => {
+    e.preventDefault();
     if (text.trim() === "") return;
 
     dispatch({
@@ -30,15 +30,15 @@ export default function TodoHome() {
     });
 
     setText("");
-  }
-  const inProgresssearch = todos
-    .filter((t) => !t.completed)
-    .filter((t) => t.text.toLowerCase().includes(searchTerm.toLowerCase()));
+  };
 
   const inProgress = todos.filter((t) => !t.completed);
   const completed = todos.filter((t) => t.completed);
   const total1 = inProgress.length;
   const total2 = completed.length;
+  const inProgresssearch = inProgress.filter((t) =>
+    t.text.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   return (
     <div className="container">
@@ -52,13 +52,13 @@ export default function TodoHome() {
       <div className="container">
         <h1 className="app-title">Welcome, {userName}💕 </h1>
       </div>
-      <h1 className="app-title">Task Manager  🐲</h1>
+      <h1 className="app-title">Task Manager 🐲</h1>
 
-      {/* ADD */}
+      {/*============================================== ADD =============================*/}
       <section className="top-sec">
         <div className="add-box">
           <input
-          ref={inputRef}
+            ref={inputRef}
             type="text"
             placeholder="What do you need to do?"
             value={text}
@@ -69,7 +69,7 @@ export default function TodoHome() {
           </button>
         </div>
 
-        {/* SEARCH */}
+        {/*====================================== SEARCH ==============================*/}
         <div className="search-box">
           <input
             type="text"
@@ -127,15 +127,15 @@ export default function TodoHome() {
             <p className="empty">No completed tasks</p>
           )}
 
-          {completed.map((t) => (
-            <div key={t.id} className="task-card completed">
-              <p className="task-text">{t.text}</p>
+          {completed.map((elem) => (
+            <div key={elem.id} className="task-card completed">
+              <p className="task-text">{elem.text}</p>
 
               <div className="task-actions">
                 <button
                   className="btn undo-btn"
                   onClick={() =>
-                    dispatch({ type: "toggle", payload: { id: t.id } })
+                    dispatch({ type: "toggle", payload: { id: elem.id } })
                   }
                 >
                   Undo
@@ -156,7 +156,7 @@ export default function TodoHome() {
         </section>
       </div>
 
-      {/* SEARCH RESULTS */}
+      {/*===================================== SEARCH RESULTS =================================*/}
       {searchTerm.trim() !== "" && (
         <div className="search-results">
           <h2 className="section-title">Search Results</h2>
